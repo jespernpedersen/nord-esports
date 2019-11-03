403b4fb603ab276ef138e5715ed6285f477d2146
Jesper N. Pedersen <jeppenp12@gmail.com>
2019-11-03 23:03:35 +0100

    Structure defined in theme / frontpage

--- wp-content/themes/obsessive-collecive/frontpage.php
+++ wp-content/themes/obsessive-collecive/frontpage.php
@@ -7,5 +7,27 @@ Template Post Type: post, page, event
 
 get_header();
 ?>
+
+<!-- Content -->
+<main>
+    <!-- Become A Player / Become A Coach -->
+    <section class="players-coach">
+        <div class="container">
+            <section class="player">
+                <h2>Become A Player</h2>
+            </section>
+            <section class="coach">
+                <h2>Become A Coach</h2>
+            </section>
+        </div>
+    </section>
+    <!-- About / Blog -->
+
+    <!-- Sponsors -->
+
+    <!-- Twitch / YouTube -->
+</main>
+
+
 <?php
 get_footer();
