a714587fb75b9889a7fc40d56121261321bb9234
Jesper Pedersen <jeppenp12@gmail.com>
2019-11-04 00:03:30 +0100

    Defined sections bare minimum styling

--- wp-content/themes/obsessive-collecive/frontpage.php
+++ wp-content/themes/obsessive-collecive/frontpage.php
@@ -14,18 +14,76 @@ get_header();
     <section class="players-coach">
         <div class="container">
             <section class="player">
-                <h2>Become A Player</h2>
+                <div class="inner">
+                    <h2>Become <br />A Player</h2>
+                    <p>Oat cake gingerbread oat cake pier bear claw tiramisu toffee souffle chocolate cake. Jelly oat cake danish croissant apple pie biscuit gummi bears.</p>
+                    <a class="btn primary" href="#" title="">Sign up</a>
+                </div>
             </section>
             <section class="coach">
-                <h2>Become A Coach</h2>
+                <div class="inner">
+                    <h2>Become <br />A Coach</h2>
+                    <p>Oat cake gingerbread oat cake pier bear claw tiramisu toffee souffle chocolate cake. Jelly oat cake danish croissant apple pie biscuit gummi bears.</p>
+                    <a class="btn primary" href="#" title="">Sign up</a>
+                </div>
             </section>
         </div>
     </section>
-    <!-- About / Blog -->
-
+    <!-- About -->
+    <section class="about">
+        <div class="container">
+            <div class="column">
+                <h2>About</h2>
+                <p>Text</p>
+                <a href="btn grey-btn" href="#" title="">Read More</a><a class="btn secondary-btn" href="#" title="">Contact Us</a>
+            </div>
+            <div class="column">
+                <img src="#" title="" alt="" /> <!-- placeholder image -->
+            </div>
+        </div>
+    </section>
+    <!-- Blog -->
+    <section class="blog" class="green-bg">
+        <div class="container">
+            <h2>Blog</h2>
+            <div class="grid-layout layout-3">
+                <div class="blog-item">
+                    <div class="bg-image">
+                        <h3>Title</h3>
+                        <p>Text</p>
+                    </div>
+                    <a href="#" class="blog-btn" title="">Read More</a>
+                </div>
+                <div class="blog-item">
+                    <div class="bg-image">
+                        <h3>Title</h3>
+                        <p>Text</p>
+                    </div>
+                    <a href="#" class="blog-btn" title="">Read More</a>
+                </div>
+                <div class="blog-item">
+                    <div class="bg-image">
+                        <h3>Title</h3>
+                        <p>Text</p>
+                    </div>
+                    <a href="#" class="blog-btn" title="">Read More</a>
+                </div>
+            </div>
+        </div>
+    </section>
     <!-- Sponsors -->
+    <section class="sponsors">
+        <div class="container">
+            <div class="sponsor-gallery">
+                <img src="#" alt="" title="" />
+            </div>
+        </div>
+    </section>
 
     <!-- Twitch / YouTube -->
+    <section class="video-platforms">
+    
+    </section>
 </main>
 
 
