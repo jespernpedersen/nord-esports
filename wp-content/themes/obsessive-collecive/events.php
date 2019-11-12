53a88dbff541aa436cf3bf8d3b126c3abe7055df
Jesper Pedersen <jeppenp12@gmail.com>
2019-11-12 17:35:24 +0100

    Don't print out featured event in all events

--- wp-content/themes/obsessive-collecive/events.php
+++ wp-content/themes/obsessive-collecive/events.php
@@ -13,6 +13,40 @@ get_header();
 
     <!-- Featured Event -->
 
+    <?php 
+
+    $posts = get_posts( array(
+        'post_type'      => 'event',
+        'order'          => 'DESC',
+        'orderby'        => 'meta_value',
+        'meta_key'       => 'is_post_featured',
+    ));
+
+    if( $posts ) {
+        foreach( $posts as $post ) {
+            // Do something.
+            // print_r($post);
+            // Category
+
+            echo "FEATURED!!! <br />"; 
+            echo get_the_category($post->ID)[0]->slug;
+            echo "<br />";
+            // Title
+            echo get_the_title($post->ID);
+            echo "<br />";
+            // Date
+            echo get_field("event_date");
+            echo "<br />";
+            // Button Link to Facebook
+            echo get_field("event_link");
+            echo "<br />";
+            // Get Image URL
+            echo get_the_post_thumbnail_url($post->ID);
+        }
+    }
+
+
+    ?>
 
     <!-- All Events -->
 
@@ -22,12 +56,14 @@ get_header();
         Date
         Button Link to Facebook
         Image
+
+        Should sort by date first
     -->
 <?php 
 
     $posts = get_posts( array(
         'post_type'      => 'event',
-        'order'          => 'ASC',
+        'order'          => 'DESC',
         'orderby'        => 'meta_value',
         'meta_key'       => 'event_date',
         'meta_type'      => 'DATETIME',
@@ -38,28 +74,32 @@ get_header();
             // Do something.
             // print_r($post);
 
+            $meta_posts = get_post_meta($post->ID);
+
+            $is_post_featured = $meta_posts["is_post_featured"][0];
+
+
+            if($is_post_featured != 1) {
+            print_r(get_post_meta($post->ID["is_post_featured"]));
+
             // Category
-            print_r(get_the_category($post->ID)[0]->slug);
-
+            echo get_the_category($post->ID)[0]->slug;
             echo "<br />";
-
             // Title
             echo get_the_title($post->ID);
-
             echo "<br />";
-
             // Date
             echo get_field("event_date");
-
-            
             echo "<br />";
-
             // Button Link to Facebook
             echo get_field("event_link");
-
             echo "<br />";
-
+            // Get Image URL
             echo get_the_post_thumbnail_url($post->ID);
+            }
+            else {
+
+            }
         }
     }
 
