bc6c7ef73ecc82814dbb9db3bbf2334c15cb5fc7
Jesper N. Pedersen <jeppenp12@gmail.com>
2019-11-14 04:07:23 +0100

    Theme Selection Support for Standard Pages

--- wp-content/themes/obsessive-collecive/page.php
+++ wp-content/themes/obsessive-collecive/page.php
@@ -1,16 +1,24 @@
 <?php 
-/*
-Template Name: Full-width layout
-Template Post Type: post, page, event
-*/
 
 
 get_header();
 ?>
-
 <!-- Content -->
-<main>
 
+<main class="<?php echo get_field("theme") ?>">
+
+
+    <!-- Page Header -->
+    <header class="page-header overlay" style="background-image: url('<?php echo get_the_post_thumbnail_url() ?>)'">
+        <h1><?php echo get_the_title(); ?></h1>
+    </header>
+
+    <!-- Content -->
+    <div id="page-content">
+        <div class="container">
+            <?php echo get_the_content(); ?>
+        </div>
+    </div>
 </main>
 
 
